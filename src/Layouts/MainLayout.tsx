ede3f758d98d58150abf52b3a7bda2799ac263c8
Valenmar03 <valenmrtnz8@gmail.com>
2025-12-05 18:20:34 -0300

    Update MainLayout to adjust main background opacity for improved readability

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -149,7 +149,7 @@ export function MainLayout() {
           </div>
         </header>
 
-        <main className="flex-1 overflow-y-auto bg-jordy-blue-50">
+        <main className="flex-1 overflow-y-auto bg-jordy-blue-50/80">
           <div className="max-w-6xl mx-auto p-4 md:p-6">
             <Outlet/>
           </div>
